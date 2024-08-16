a7fdb8f8a66693326d4b96698399349f51d1f240
Young-zai <48758181+Young-zai@users.noreply.github.com>
2024-08-16 15:22:07 +0800

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -1,6 +1,10 @@
 
 
 export default async function handler(req, res) {
+  // 添加 CORS 头信息
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'POST') {
     const client = req.body.ShopifyBuy.buildClient({
       domain: process.env.SHOP_DOMAIN,
